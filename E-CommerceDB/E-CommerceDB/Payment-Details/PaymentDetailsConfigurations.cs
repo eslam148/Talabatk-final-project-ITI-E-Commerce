bee2522f3591ae0b86f5d27587abbdb8263e679c
amira <amirakenady2020@gmail.com>
2022-11-22 21:28:33 +0200

    add orderitems and payment details folders

---
+++ E-CommerceDB/E-CommerceDB/Payment-Details/PaymentDetailsConfigurations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceDB.Payment_Details
+{
+    internal class PaymentDetailsConfigurations
+    {
+    }
+}
