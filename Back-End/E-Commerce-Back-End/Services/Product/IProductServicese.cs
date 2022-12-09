e793e80805d82d9fc5c1689bbd4dac49f0655310
amira <amirakenady2020@gmail.com>
2022-12-09 12:45:22 +0200

    Add getBestSeller Service & getNewProducts & addSellerProducts

--- Back-End/E-Commerce-Back-End/Services/Product/IProductServicese.cs
+++ Back-End/E-Commerce-Back-End/Services/Product/IProductServicese.cs
@@ -17,6 +17,9 @@ namespace E_Commerce_Back_End
         public IEnumerable<ProductsVM> GetProductBySubCatPriceRange(int SubCat_id,int start_price,int end_price);
         public IEnumerable<ProductsVM> GetProductByCatAndPrice(int CatID, int start_price, int end_price);
         public Task<IEnumerable<ProductsVM>> GetProductByCategory(int CatID);
+        public void AddSellerProdcuts(ProductsVM product);
+        public IEnumerable<ProductsVM> GetNewProducts();
+        public IEnumerable<ProductsVM> GetBestSeller();
 
     }
 }
