1b7822ad6f997ebe5ab02a569212944c4ba33ec6
Moohamedelnoby <elnobymohamed51@gmail.com>
2022-12-09 17:00:02 +0200

    category service

--- Back-End/E-Commerce-Back-End/Controllers/CategoryController.cs
+++ Back-End/E-Commerce-Back-End/Controllers/CategoryController.cs
@@ -7,10 +7,12 @@ namespace E_Commerce_Back_End.Controllers
     public class CategoryController : ControllerBase
     {
         private readonly ICategory ICategory;
-        public CategoryController(ICategory _ICategory)
+        private readonly ISubcategory ISubCategory;
+        public CategoryController(ICategory _ICategory, ISubcategory _ISubCategory)
         {
 
             ICategory = _ICategory;
+            ISubCategory = _ISubCategory;
         }
         [HttpGet]
         public async Task<IActionResult> Get()
@@ -23,6 +25,30 @@ namespace E_Commerce_Back_End.Controllers
 
             return NotFound();
         }
-       
+        [HttpGet]
+        [Route("~/api/GetSubCategory/{id}")]
+        public IActionResult GetSubCategory(int id)
+        {
+            var res = ISubCategory.GetSubcategoryByCategoryId(id);
+            if (res != null)
+            {
+                return Ok(res);
+            }
+
+            return NotFound();
+        }
+        [HttpGet]
+        [Route("~/api/GetCategoryById/{id}")]
+        public IActionResult GetCategoryById(int id)
+        {
+            var res = ICategory.get(id);
+            if (res != null)
+            {
+                return Ok(res);
+            }
+
+            return NotFound();
+        }
+
     }
 }
